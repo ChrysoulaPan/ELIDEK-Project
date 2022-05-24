b6cb97c22c4adf3cdef81070b9ce5e822a7408bb
ChrysoulaPan <61976427+ChrysoulaPan@users.noreply.github.com>
2022-05-24 13:17:58 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+//import dependencies
+const express = require('express');
+const path = require('path');
+const db = require('./database');
+const PORT = process.env.PORT || 3000;
+
+//import routes
+const first = require('../router/first');
+const second = require('../router/second');
+
+//start the express app
+const app = express();
+
+//import middlewares
+app.get('/', (req, res) => res.send('Node App is running'));
+app.use('/', first);
+app.use('/', second);
+
+//connect to database once app is started
+db.connect((err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('connected')
+  });
+
+  //make the connection global
+  global.db = db;
+  
+  
+  //to keep the connection alive, make frequent quries to SQL database
+  setInterval(function () {
+    
+      db.query('SELECT 1');
+  }, 5000);
+   
+
+app.listen(PORT, ()=> console.log(`WebApp running on port  ${PORT}`));
+
+module.exports = app
