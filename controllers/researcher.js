ebf9d0a9a92aaa8349dc44fe1293a21eaf42d52a
ChrysoulaPan <61976427+ChrysoulaPan@users.noreply.github.com>
2022-05-26 23:46:59 +0300

    Add files via upload

--- controllers/researcher.js
+++ controllers/researcher.js
@@ -3,16 +3,17 @@ const { pool } = require('../config/config');
 /* controller to retrieve researchers from database */
 exports.getResearcher = (req, res, next) => {
 
+
     /* create the connection, execute query, render data */
     pool.getConnection((err, conn) => {
+        
         conn.promise().query('SELECT researcher_id, researcher_name, researcher_surname, birth_date FROM researcher')
         .then(([rows, fields]) => {
             res.render('researcher.ejs', {
-                pageTitle: "Researcher Page",
-                researcher: rows
+                pageTitle: "Researchers Page",
+                researcher: rows,
             })
         })
-        .then(() => console.log('Query is running'))
         .then(() => pool.releaseConnection(conn))
         .catch(err => console.log(err))
     })
