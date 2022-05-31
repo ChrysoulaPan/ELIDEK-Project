a4cb760b22c3b1f6ea76261ef64a565e608e4201
ChrysoulaPan <61976427+ChrysoulaPan@users.noreply.github.com>
2022-05-31 19:08:50 +0300

    Add files via upload

--- controllers/fields.js
+++ controllers/fields.js
@@ -13,7 +13,6 @@ exports.getFields = (req, res, next) => {
         res.render('fields.ejs', {
             ids: id_array,
             names: name_array,
-            len: id_array.length
         });
     }
     
@@ -30,42 +29,79 @@ exports.getFields = (req, res, next) => {
             setOutput(rows);
         });
     });    
+
 }
 
-function showProjectFields (value) {
+exports.showInField = (req, res, next) => {
 
-    const view_active = 'drop view if exists active_projects; \
-    create view active_projects as \
-    select project_id, project_title, fund_ammount \
-    from project \
-    where start_date <= curdate() and end_date > curdate();'
+    let proj_ids = [], proj_titles = [];
+    let res_ids = [], res_names = [], res_surnames = [];
 
-    const view_proj = 'drop view if exists projects_in_field_f; \
-    create view projects_in_field_f as \
-    select p.project_id, p.project_title from active_projects p \
-    inner join project_field pf on p.project_id = pf.project_id \
-    where pf.field_id = ' + value + 'and p.fund_ammount >= 0;'
+        var value = req.params.value;
+    
+        const view_active = 'drop view if exists active_projects; \
+        create view active_projects as \
+        select project_id, project_title, fund_ammount \
+        from project \
+        where start_date <= curdate() and end_date > curdate();';
+    
+        const proj_field = 'select p.project_id, p.project_title from active_projects p \
+        inner join project_field pf on p.project_id = pf.project_id \
+        where pf.field_id = ' + value + ' \
+        order by p.project_id';
+    
+        const res_field = 'select r.researcher_id, r.researcher_name, r.researcher_surname from researcher r \
+        inner join researcher_works_on rwo on r.researcher_id = rwo.researcher_id \
+        inner join active_projects p on p.project_id = rwo.project_id \
+        inner join project_field pf on p.project_id = pf.project_id \
+        where pf.field_id = ' + value + ' \
+        order by r.researcher_id';
 
-    const res_field = 'select r.researcher_id, r.researcher_name, r.researcher_surname from researcher r \
-    inner join researcher_works_on rwo on r.researcher_id = rwo.researcher_id \
-    inner join active_projects p on p.project_id = rwo.project_id \
-    inner join project_field pf on p.project_id = pf.project_id \
-    where pf.field_id = ' + value + ';'
+        function fillProj (rows) {
+            for (var i = 0; i < rows.length; i++) {
+                proj_ids.push(rows[i].project_id);
+                proj_titles.push(rows[i].project_title);
+            }
+        }
 
-    pool.getConnection((err, conn) => {
-        
-        conn.promise().query(view_active)
-        conn.promise().query(view_proj)
+        function fillRes (rows) {
+            for (var i = 0; i < rows.length; i++) {
+                res_ids.push(rows[i].researcher_id);
+                res_names.push(rows[i].researcher_name);
+                res_surnames.push(rows[i].researcher_surname);
+            }
+        }
 
-        conn.promise().query(res_field)
-        .then(([rows, fields]) => {
-            res.render('young_res.ejs', {
-                pageTitle: "Young Researchers",
-                young_res: rows,
+        pool.getConnection((err, conn) => {
+    
+            let ProjectPromise = new Promise((resolve, reject) => {
+                conn.promise().query(view_active)
+                conn.promise().query(proj_field)
+                .then(([rows, fields]) => {
+                    fillProj(rows);
+                    resolve();
+                 })
+                .then(() => pool.releaseConnection(conn))
+                .catch(err => console.log(err))
             })
+    
+            let ResearcherPromise = new Promise((resolve, reject) => {
+                conn.promise().query(view_active)
+                conn.promise().query(res_field)
+                .then(([rows, fields]) => {
+                    fillRes(rows);
+                    resolve();
+                 })
+                .then(() => pool.releaseConnection(conn))
+                .catch(err => console.log(err))
+            })
+    
+            Promise.all([ProjectPromise, ResearcherPromise]).then(() => {
+                res.render('field_res.ejs', {
+                    pageTitle: "Fields Page",
+                    proj_ids, proj_titles,
+                    res_ids, res_names, res_surnames
+                })
+            });
         })
-        .then(() => pool.releaseConnection(conn))
-        .catch(err => console.log(err))
-    })
-}
- 
+    }
