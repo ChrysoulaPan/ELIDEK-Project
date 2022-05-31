3fb31793873f8a5e9a21ce7ff44b4af95b0775a1
ChrysoulaPan <61976427+ChrysoulaPan@users.noreply.github.com>
2022-05-31 20:26:50 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const executives = require('./router/executives');
 const young_res = require('./router/young_res');
 const no_del = require('./router/no_del');
 const fields = require('./router/fields');
+const annual = require('./router/annual_proj');
+const tuples = require('./router/tuples');
+//const dash = require('/router/dash');
 
 
 //start the express app
@@ -20,12 +23,16 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 //import middlewares
-app.get('/', (req, res) => res.render('fields.ejs'));
+app.get('/', (req, res) => res.render('fields.ejs'));  //εδω θα μπει το αρχειο της μαριλινας ομως σε ejs
 app.use('/researchers', researchers);
 app.use('/executives', executives);
 app.use('/young', young_res);
 app.use('/nodeliverables', no_del);
 app.use('/fields', fields);
+app.use('/annual_proj', annual);
+app.use('/tuples', tuples);
+//app.use('/dashboard', dash);
+
 
 // in case an endpoint does not exist must return 404.html
 app.use((req, res, next) => { res.status(404).render('404.ejs', { pageTitle: '404' }) })
