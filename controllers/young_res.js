64a8db7db34723f766104d9fa89816fddfe22169
MarilinaBarts <106103252+MarilinaBarts@users.noreply.github.com>
2022-06-03 21:08:29 +0300

    Add files via upload

--- controllers/young_res.js
+++ controllers/young_res.js
@@ -2,6 +2,12 @@ const { pool } = require('../config/config');
 
 exports.getYoungRes = (req, res, next) => {
 
+    const view_active_projects = 'drop view if exists active_projects; \
+    create view active_projects as \
+    select project_id, project_title, fund_ammount \
+    from project \
+    where start_date <= curdate() and end_date > curdate();'
+    
     const view_young_res = 'drop view if exists young_researchers; \
     create view young_researchers as \
     select researcher_id, researcher_name, researcher_surname, extract(year from from_days(age)) as age_in_years \
@@ -11,6 +17,7 @@ exports.getYoungRes = (req, res, next) => {
     const q3_6 = 'select r.researcher_id, r.researcher_name, r.researcher_surname, count(*) as proj_count \
     from young_researchers r \
     inner join researcher_works_on rwo on r.researcher_id = rwo.researcher_id \
+    inner join active_projects a on a.project_id = rwo.project_id \
     group by r.researcher_id \
     order by proj_count desc \
     limit 10;'
