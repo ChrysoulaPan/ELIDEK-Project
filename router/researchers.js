91e8d1e7eb1201b87738dee801549986c1dfd7dd
ChrysoulaPan <61976427+ChrysoulaPan@users.noreply.github.com>
2022-06-05 00:00:15 +0300

    Add files via upload

--- router/researchers.js
+++ router/researchers.js
@@ -4,5 +4,9 @@ const researcherController = require('../controllers/researchers');
 const router = express.Router();
 
 router.get('/', researcherController.getResearcher);
+router.post('/create', researcherController.postResearcher);
+router.post('/update/:id', researcherController.postUpdateResearcher);
+router.post('/delete/:id', researcherController.postDeleteResearcher);
+
 
 module.exports = router;
