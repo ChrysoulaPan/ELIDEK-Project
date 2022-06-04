31ee2b3855df97708d694ad2b5c66f2d6ddc6c43
MarilinaBarts <106103252+MarilinaBarts@users.noreply.github.com>
2022-06-04 21:39:46 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 //import routes
 const researchers = require('./router/researchers');
-const organizations = require('./router/organizations');
 const executives = require('./router/executives');
 const young_res = require('./router/young_res');
 const no_del = require('./router/no_del');
@@ -15,7 +14,10 @@ const dash = require('./router/dash');
 const programs = require('./router/programs');
 const res_view = require('./router/res_view');
 const org_view = require('./router/org_view');
-
+const organizations = require('./router/organizations');
+const exec = require('./router/exec');
+const scientific = require('./router/scientific');
+const prog = require('./router/prog');
 
 //start the express app
 const app = express();
@@ -29,9 +31,8 @@ app.set('views', 'views');
 //import middlewares
 app.get('/', (req, res) => res.render('website.ejs'));  
 app.use('/researchers', researchers);
-app.use('/organizations', organizations);
 app.use('/executives', executives);
-app.use('/young', young_res);
+app.use('/young_res', young_res);
 app.use('/nodeliverables', no_del);
 app.use('/fields', fields);
 app.use('/annual_proj', annual);
@@ -40,7 +41,10 @@ app.use('/dashboard', dash);
 app.use('/programs', programs);
 app.use('/res_view', res_view);
 app.use('/org_view', org_view);
-
+app.use('/organizations', organizations);
+app.use('/exec', exec);
+app.use('/scientific', scientific);
+app.use('/prog', prog);
 
 // in case an endpoint does not exist must return 404.html
 app.use((req, res, next) => { res.status(404).render('404.ejs', { pageTitle: '404' }) })
